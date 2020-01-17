ffae797a22613d5f646315495c4f70137ba71ad9
penggehero <1571566616@qq.com>
2020-01-17 12:21:31 +0800

    *）完成90% by wupeng

--- src/views/student/paper.vue
+++ src/views/student/paper.vue
@@ -3,20 +3,10 @@
     <!-- 题目区域 -->
     <div class="nr_left">
       <div class="test">
-        <!-- 下方提示栏 -->
-        <div class="test_title">
-          &nbsp;&nbsp; &nbsp;&nbsp;
-          <strong style="color:red;font-size: 15px;">距离考试结束还剩:</strong> &nbsp;&nbsp;
-          <span style="color:red;font-size: 20px">
-            01:40
-          </span>
-          <span class="test_sumbit">
-            <el-button type="text" style="color: #fff;font-size: 18px">交卷</el-button>
-          </span>
-        </div>
+
         <!-- 标题 -->
         <div class="test_content">&nbsp;&nbsp;&nbsp;&nbsp;
-          <el-button type="primary" autofocus="false" round>单选题</el-button>
+          <el-button type="primary" round>单选题</el-button>
           <span style="font-size: 14px;"> &nbsp;&nbsp; &nbsp;&nbsp;
             <span>共&nbsp;<span>60</span>&nbsp;题，</span>
             <span>合计&nbsp;</span><span>60</span><span>&nbsp;分</span>
@@ -25,23 +15,62 @@
       </div>
       <!--  题目-->
       <div>
-        <div v-for="(item,index) in dataList">
+        <!-- 单选 -->
+        <div v-for="(item,index) in singleList">
           <div :id="'q_s_'+index" style="margin-left:2%;margin-top:10px">
             <el-tag color="#409EFF" effect="dark" style="color:white">{{ index+1 }}</el-tag> &nbsp;&nbsp;
             <span style="font-size: 10px;">({{ item.mark }}分)</span>
             <label>{{ item.name }}</label>
           </div>
-          <el-radio-group v-model="answerList[index]" class="nr_question">
-            <div><el-radio label="A">A. {{ item.q_A }}</el-radio> </div>
-            <div> <el-radio label="B">B.{{ item.q_B }}</el-radio> </div>
-            <div><el-radio label="C">C. {{ item.q_C }}</el-radio></div>
-            <div><el-radio label="D">D.{{ item.q_D }}</el-radio></div>
+          <el-radio-group v-model="singleAnswerList[index]" class="nr_question">
+            <div><el-radio :label="'A'+'|'+item.id">A. {{ item.q_A }}</el-radio> </div>
+            <div><el-radio :label="'B'+'|'+item.id">B.{{ item.q_B }}</el-radio> </div>
+            <div><el-radio :label="'C'+'|'+item.id">C. {{ item.q_C }}</el-radio></div>
+            <div><el-radio :label="'D'+'|'+item.id">D.{{ item.q_D }}</el-radio></div>
           </el-radio-group>
           <hr>
         </div>
+        <div class="test">
+          <!-- 标题 -->
+          <div class="test_content">&nbsp;&nbsp;&nbsp;&nbsp;
+            <el-button type="primary" round>多选题</el-button>
+            <span style="font-size: 14px;"> &nbsp;&nbsp; &nbsp;&nbsp;
+              <span>共&nbsp;<span>60</span>&nbsp;题，</span>
+              <span>合计&nbsp;</span><span>60</span><span>&nbsp;分</span>
+            </span>
+          </div>
+          <!-- 多选 -->
+          <div v-for="(item,index) in doubleList">
+            <div :id="'q_s_'+index" style="margin-left:2%;margin-top:10px">
+              <el-tag color="#409EFF" effect="dark" style="color:white">{{ index+1 }}</el-tag> &nbsp;&nbsp;
+              <span style="font-size: 10px;">({{ item.mark }}分)</span>
+              <label>{{ item.name }}</label>
+            </div>
+            <el-checkbox-group v-model="doubleAnswerList[index]" class="nr_question_d">
+              <div><el-checkbox :label="'A'+'|'+item.id">A. {{ item.q_A }}</el-checkbox> </div>
+              <div><el-checkbox :label="'B'+'|'+item.id">B.{{ item.q_B }}</el-checkbox> </div>
+              <div><el-checkbox :label="'C'+'|'+item.id">C. {{ item.q_C }}</el-checkbox></div>
+              <div><el-checkbox :label="'D'+'|'+item.id">D.{{ item.q_D }}</el-checkbox></div>
+            </el-checkbox-group>
+            <hr>
+          </div>
+        </div>
+        <br><br><br><br>
+        <!-- 下方提示栏 -->
+        <div class="test_title">
+          &nbsp;&nbsp; &nbsp;&nbsp;
+          <strong style="color:red;font-size: 15px;">距离考试结束还剩:</strong> &nbsp;&nbsp;
+          <span style="color:red;font-size: 20px">
+            <strong>{{ timeSet.hour }}:{{ timeSet.minute }}:{{ timeSet.second }}</strong>
+          </span>
+          <span class="test_sumbit">
+            <el-button type="text" style="color: #fff;font-size: 18px">交卷</el-button>
+          </span>
+        </div>
       </div>
     </div>
-    {{ answerList }}
+    {{ singleAnswerList }}
+    {{ doubleAnswerList }}
     <!-- 答题卡区域 -->
     <div class="nr_right">
       <div class="nr_rt_main">
@@ -61,7 +90,7 @@
             </div>
             <div class="answerSheet">
               <ul>
-                <li v-for="(item,index) in dataListLength">
+                <li v-for="(item,index) in singleLength" :id="'li_s_'+index">
                   <a :href="'#q_s_'+index">{{ item }}</a>
                 </li>
               </ul>
@@ -75,12 +104,9 @@
             </div>
             <div class="answerSheet">
               <ul>
-                <li><a href="#qu_0_0">1</a></li>
-                <li><a href="#qu_0_1">2</a></li>
-                <li><a href="#qu_0_2">3</a></li>
-                <li><a href="#qu_0_2">4</a></li>
-                <li><a href="#qu_0_2">5</a></li>
-                <li><a href="#qu_0_2">6</a></li>
+                <li v-for="(item,index) in doubleLength" :id="'li_d_'+index">
+                  <a :href="'#q_s_'+index">{{ item }}</a>
+                </li>
               </ul>
             </div>
           </div>
@@ -95,71 +121,16 @@ export default {
   name: 'Paper',
   data: () => {
     return {
-      radio: '',
-      dataListLength: 2,
-      dataList: [
+      singleLength: 0,
+      doubleLength: 0,
+      intDiff: 30,
+      timeSet: {
+        hour: 0,
+        minute: 0,
+        second: 0// 时间默认值
+      },
+      singleList: [
         {
-          id: 1,
-          p_id: 1,
-          number: 1,
-          name: '1+1=?',
-          mark: 1,
-          q_A: '2',
-          q_B: '3',
-          q_C: '-1',
-          q_D: '不知道'
-        },
-        {
-          id: 2,
-          p_id: 1,
-          number: 2,
-          name: '1+2=?',
-          mark: 1,
-          q_A: '2',
-          q_B: '3',
-          q_C: '-1',
-          q_D: '不知道'
-        }, {
-          id: 1,
-          p_id: 1,
-          number: 1,
-          name: '1+1=?',
-          mark: 1,
-          q_A: '2',
-          q_B: '3',
-          q_C: '-1',
-          q_D: '不知道'
-        }, {
-          id: 1,
-          p_id: 1,
-          number: 1,
-          name: '1+1=?',
-          mark: 1,
-          q_A: '2',
-          q_B: '3',
-          q_C: '-1',
-          q_D: '不知道'
-        }, {
-          id: 1,
-          p_id: 1,
-          number: 1,
-          name: '1+1=?',
-          mark: 1,
-          q_A: '2',
-          q_B: '3',
-          q_C: '-1',
-          q_D: '不知道'
-        }, {
-          id: 1,
-          p_id: 1,
-          number: 1,
-          name: '1+1=?',
-          mark: 1,
-          q_A: '2',
-          q_B: '3',
-          q_C: '-1',
-          q_D: '不知道'
-        }, {
           id: 1,
           p_id: 1,
           number: 1,
@@ -171,7 +142,59 @@ export default {
           q_D: '不知道'
         }
       ],
-      answerList: []
+      doubleList: [
+        {
+          id: 1,
+          p_id: 1,
+          number: 1,
+          name: '1+1=?',
+          mark: 1,
+          q_A: '2',
+          q_B: '3',
+          q_C: '-1',
+          q_D: '不知道'
+        }
+      ],
+      singleAnswerList: [],
+      doubleAnswerList: [[]]
+    }
+  }, watch: {
+    singleAnswerList(val) {
+      val.forEach((value, index) => {
+        document.getElementById('li_s_' + index).className = 'hasBeenAnswer'
+      })
+    },
+    doubleAnswerList(val) {
+      val.forEach((value, index) => {
+        document.getElementById('li_d_' + index).className = 'hasBeenAnswer'
+      })
+    }
+  },
+  created() {
+    this.search()
+    this.singleLength = this.singleList.length
+    this.doubleLength = this.doubleList.length
+    //this.timer()
+  },
+  methods: {
+    search() {
+
+    },
+    timer() {
+      window.setInterval(() => {
+        this.intDiff--
+        if (this.intDiff <= 0) {
+          alert('考试已结束!')
+        }
+        if (this.intDiff > 0) {
+          this.timeSet.hour = Math.floor(this.intDiff / (60 * 60))
+          this.timeSet.minute = Math.floor(this.intDiff / 60) - (this.timeSet.hour * 60)
+          this.timeSet.second = this.intDiff - (this.timeSet.hour * 60 * 60) - (this.timeSet.minute * 60)
+          if (this.timeSet.hour <= 9) { this.timeSet.hour = '0' + this.timeSet.hour }
+          if (this.timeSet.minute <= 9) { this.timeSet.minute = '0' + this.timeSet.minute }
+          if (this.timeSet.second <= 9) { this.timeSet.second = '0' + this.timeSet.second }
+        }
+      }, 1000)
     }
   }
 }
@@ -240,9 +263,12 @@ export default {
   margin-left: 70%;
   margin-top: 10px;
 }
+.nr_question_d div{
+  margin-left: 6%;
+  margin-top: 10px;
+}
 .test {
     width: 100%;
-    border: 1px solid #e4e4e4;
     text-align: left;
     float: left;
 }
@@ -253,7 +279,7 @@ export default {
     background-color: #f7f7f7;
     position: fixed;
     bottom: 0;
-    z-index: 1000;
+    margin-top: 1px;
 }
 .test_content {
     width: 1000px;
